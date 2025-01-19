b089be17b0e5bc1ce40e52f7572ca49a219e4586
[winsam] <winsam003@gmail.com>
2025-01-20 02:39:01 +0900

    1페이지 수정정

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,8 +2,15 @@ import "../css/Header.css"
 
 export default function Header() {
     return (
-        <div className = "Header_Continor">
-            <div className="Header_Box">Header</div>
+        <div className="Header_Continor">
+            <div className="Header_Box">
+                <div className="Header_icon">최승삼's 포트폴리오</div>
+                <div className="Header_menu">
+                    <span className="Header_menuTap Header_menu1">About Me</span>
+                    <span className="Header_menuTap Header_menu2">Career</span>
+                    <span className="Header_menuTap Header_menu3">Projects</span>
+                </div>
+            </div>
         </div>
     )
 }
--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -4,7 +4,12 @@ export default function Main() {
     return (
         <div className="Main_Containor">
             <div className="Main_Box">
-                1
+                <div className="Main_Box1">
+                    <div className="Main_Box1Fir">- 최승삼 -</div>
+                    <div className="Main_Box1Sec">웹 개발자 포트폴리오</div>
+                </div>
+                <hr />
+                <div className="Main_Box2">안녕하세요. 프론트, 백 웹 개발자 최승삼입니다.</div>
             </div>
         </div>
     )
