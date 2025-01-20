f3dc29337c0913782d162094ed2743d923f3823f
yoonsaeyoon <ylyy44@urbanlt.co.kr>
2025-01-20 14:55:26 +0900

    1페이지 추가작업 완료

--- src/component/AboutMe.jsx
+++ src/component/AboutMe.jsx
@@ -4,7 +4,18 @@ export default function AboutMe(){
     return (
         <div className="AboutMe_Containor">
             <div className="App_CommCenter">
-                2
+                <div className="AboutMe_FirBox">
+                    <span>🔗</span>
+                    <span>ABOUT ME</span>
+                </div>
+                <div className="AboutMe_SecBox">
+                    <span>1</span>
+                    <span>2</span>
+                    <span>3</span>
+                    <span>4</span>
+                    <span>5</span>
+                    <span>6</span>
+                </div>
             </div>
         </div>
     )
--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -9,7 +9,11 @@ export default function Main() {
                     <div className="Main_Box1Sec">웹 개발자 포트폴리오</div>
                 </div>
                 <hr />
-                <div className="Main_Box2">안녕하세요. 프론트, 백 웹 개발자 최승삼입니다.</div>
+                <div className="Main_Box2">
+                    <div className="Main_Box2Fir">안녕하세요.</div>
+                    <div className="Main_Box2Sec">프론트, 백 웹 개발자 최승삼입니다.</div>
+                    <span className="Main_Box2Thi">더 알아보기 ↓</span>
+                </div>
             </div>
         </div>
     )
